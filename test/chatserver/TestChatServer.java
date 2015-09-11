dda59005b0ab2b0873f49f1645f7fc0cf700357f
JanusGjoe <janusgjoe@hotmail.com>
2015-09-11 23:13:20 +0200

    ...

--- test/chatserver/TestChatServer.java
+++ test/chatserver/TestChatServer.java
@@ -1,3 +1,4 @@
+
 package chatserver;
 
 import java.io.IOException;
@@ -9,76 +10,66 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class TestChatServer {
-
+public class TestChatServer
+{
     @BeforeClass
-    public static void setUpClass() {
-        new Thread(new Runnable() {
+    public static void setUpClass()
+    {
+        new Thread(new Runnable()
+        {
             @Override
-            public void run() {
+            public void run()
+            {
                 ChatServer.main(null);
             }
         }).start();
     }
-
+    
     @AfterClass
-    public static void tearDownClass() {
+    public static void tearDownClass()
+    {
         ChatServer.stopServer();
     }
-
-//    @Test
-//    public void testConnect() throws IOException {
-//        Socket socket = new Socket("localhost", 9090);
-//        PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-//        Scanner input = new Scanner(socket.getInputStream());
-//
-//        output.println("USER#LARS");
-//        Assert.assertEquals("USERLIST#LARS", input.nextLine());
-//        
-//        Socket socket2 = new Socket("localhost", 9090);
-//        PrintWriter output2 = new PrintWriter(socket2.getOutputStream(), true);
-//        Scanner input2 = new Scanner(socket2.getInputStream());
-//
-//        output2.println("USER#THOMAS");
-//        Assert.assertEquals("USERLIST#LARS,THOMAS", input2.nextLine());
-//    }
     
     @Test
-    public void testMessage() throws IOException {
-        
-        // START 1....
+    public void testConnectMessage() throws IOException
+    {
+        // START A
         Socket socket = new Socket("localhost", 9090);
         PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
         Scanner input = new Scanner(socket.getInputStream());
         
-        // IN
+        // IN A1
         output.println("USER#LARS");
         String text = input.nextLine();
         System.out.println(text);
-        // OUT
+        // OUT A1
         Assert.assertEquals("USERLIST#LARS", text);
+        
+        // IN A2
         output.println("MSG#*#Hejjj");
         String text2 = input.nextLine();
         System.out.println(text2);
-        
+        // OUT A2
         Assert.assertEquals("MSG#LARS#Hejjj", text2);
         
-        // START 2
+        // START B
         Socket socket2 = new Socket("localhost", 9090);
         PrintWriter output2 = new PrintWriter(socket2.getOutputStream(), true);
         Scanner input2 = new Scanner(socket2.getInputStream());
         
-        // IN 2
+        // IN B1
         output2.println("USER#THOMAS");
         String text3 = input2.nextLine();
         System.out.println(text3);
-        // OUT.
+        // OUT B1
         Assert.assertEquals("USERLIST#LARS,THOMAS", text3);
+        
+        // IN B2
         output2.println("MSG#*#Hej Alle");
         String text4 = input2.nextLine();
         System.out.println(text4);
-        
+        // OUT B2
         Assert.assertEquals("MSG#THOMAS#Hej Alle", text4);
-       
     }
 }
