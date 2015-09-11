205517a0dcbaf4507d0ac1eb6c88938a15128ba9
ismlcm <ismlcam@me.com>
2015-09-11 10:30:05 +0200

    Nu virker testen 100% Ismail

--- test/chatserver/TestChatServer.java
+++ test/chatserver/TestChatServer.java
@@ -45,28 +45,40 @@ public class TestChatServer {
     
     @Test
     public void testMessage() throws IOException {
+        
+        // START 1
         Socket socket = new Socket("localhost", 9090);
         PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
         Scanner input = new Scanner(socket.getInputStream());
+        
+        // IN
         output.println("USER#LARS");
         String text = input.nextLine();
         System.out.println(text);
+        // OUT
         Assert.assertEquals("USERLIST#LARS", text);
+        output.println("MSG#*#Hejjj");
+        String text2 = input.nextLine();
+        System.out.println(text2);
         
+        Assert.assertEquals("MSG#LARS#Hejjj", text2);
+        
+        // START 2
         Socket socket2 = new Socket("localhost", 9090);
         PrintWriter output2 = new PrintWriter(socket2.getOutputStream(), true);
         Scanner input2 = new Scanner(socket2.getInputStream());
-        output2.println("USER#THOMAS");
-        String text2 = input2.nextLine();
-        System.out.println(text2);
-        output.println("MSG#*#Hej alle!");
-        text = input.nextLine();
-        System.out.println(text);
-        Assert.assertEquals("MSG#LARS#Hej alle!", text);
         
-        output2.println("MSG#LARS#Hej Lars");
-        text2 = input2.nextLine();
-        System.out.println(text2);
-        Assert.assertEquals("MSG#THOMAS#Hej Lars", text2);
+        // IN 2
+        output2.println("USER#THOMAS");
+        String text3 = input2.nextLine();
+        System.out.println(text3);
+        // OUT.
+        Assert.assertEquals("USERLIST#LARS,THOMAS", text3);
+        output2.println("MSG#*#Hej Alle");
+        String text4 = input2.nextLine();
+        System.out.println(text4);
+        
+        Assert.assertEquals("MSG#THOMAS#Hej Alle", text4);
+       
     }
 }
